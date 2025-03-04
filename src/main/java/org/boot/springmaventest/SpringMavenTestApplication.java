43d9ed6bbe7a6c843e524464cf4126feebc3a759
lepe99 <kr_@kakao.com>
2025-03-04 15:19:10 +0900

    first commit

---
+++ src/main/java/org/boot/springmaventest/SpringMavenTestApplication.java
@@ -0,0 +1,13 @@
+package org.boot.springmaventest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenTestApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMavenTestApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/org/boot/springmaventest/SpringMavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boot.springmaventest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenTestApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
